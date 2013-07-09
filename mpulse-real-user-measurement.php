05d14a7df446d1e64ab15fc75662a59db2532929
cliffcrocker <cliffcrocker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-09 17:29:22 +0000

    Fixed indentations, added key validation, css tuning
    git-svn-id: https://plugins.svn.wordpress.org/mpulse-real-user-measurement/trunk@738380 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mpulse-real-user-measurement.php
+++ mpulse-real-user-measurement.php
@@ -7,33 +7,36 @@ Version: 1.0
 Author: SOASTA
 */
 
+// Make sure we don't expose any info if called directly
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+
 //Clean up after ourselves on delete
 function mpulse_cleanup() {
 	delete_option('mpulse_api_key');
 }
 
-//Add submenu to plugin menue
+//Add submenu to plugin menu
 function mpulse_plugin_config() {
 	add_submenu_page( 'plugins.php', 'mPulse Real User Measurement Configuration', 'mPulse RUM', 'manage_options', 'mpulse_plugin_page', 'mpulse_plugin_page' );
 }
 
 //Add plugin action link to Plugin page for mPulse Settings
 function  mpulse_plugin_action_links($links, $file) {
-    static $this_plugin;
+	static $this_plugin;
 
-    if (!$this_plugin) {
-        $this_plugin = plugin_basename(__FILE__);
-    }
+	if (!$this_plugin) {
+		$this_plugin = plugin_basename(__FILE__);
+	}
 
-    if ($file == $this_plugin) {
-        // The "page" query string value must be equal to the slug
-        // of the Settings admin page we defined earlier, which in
-        // this case equals "myplugin-settings".
-        $settings_link = '<a href="' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=mpulse_plugin_page">Settings</a>';
-        array_unshift($links, $settings_link);
-    }
+	if ($file == $this_plugin) {
+		$settings_link = '<a href="' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=mpulse_plugin_page">Settings</a>';
+		array_unshift($links, $settings_link);
+	}
 
-    return $links;
+	return $links;
 }
 
 //Show mPulse Settings page
@@ -45,11 +48,15 @@ function mpulse_plugin_page() {
 	wp_enqueue_style("soasta_wp", plugin_dir_url("/", __FILE__) . trim(dirname(plugin_basename(__FILE__)), '/') . "/soasta_wp.css");
 
 	if (isset($_POST['submit'])) {
-        $mp_key = trim($_POST['mpulse_api_key']);
-        update_option('mpulse_api_key', $mp_key);
-        ?><div id="footer" class="soasta_footer"> <p>Your key has been updated.</p></div><?php
+		$mp_key = trim($_POST['mpulse_api_key']);
+		$mp_pattern = '/[A-Z0-9]{5}\-[A-Z0-9]{5}\-[A-Z0-9]{5}\-[A-Z0-9]{5}\-[A-Z0-9]{5}/';
+		if (preg_match($mp_pattern, $mp_key)) {
+			update_option('mpulse_api_key', $mp_key);
+			echo "<div id=\"footer\" class=\"soasta_footer\"> <p>Your key has been updated.</p></div>";
+		} else {
+			echo "<div id=\"footer\" class=\"soasta_footer\"> <p>Your API key is invalid! Format: A1A1A-B2B2B-C3C3C-D4D4D </p></div>";
+		}
 	}
-
 ?>
 
 <div id='container' class='soasta_container'>
@@ -57,7 +64,7 @@ function mpulse_plugin_page() {
 <img src="<?php echo plugin_dir_url("/", __FILE__) . trim(dirname(plugin_basename(__FILE__)), '/'); ?>/soasta_logo.png" alt="SOASTA Logo" />
 <h1>mPulse - Real User Measurement</h1>
 
-<p>Please enter your API KEY below. This can be found in your domain configuration within mPulse. If you do not yet have an mPulse account, you can set this up <a href="http://www.soasta.com/free" target="new">here.</a> (For FREE!).</p>
+<p>Please enter your API KEY below. This can be found in your domain configuration within mPulse. If you do not yet have an mPulse account, you can <a href="http://www.soasta.com/free" target="new" title="Create a free mPulse account">set one up (For FREE!)</a></p>
 		<form method="post" action="" class="soasta_form">
 			<input name="mpulse_api_key" type="text" id="mpulse_api_key" class="soasta_input" value="<?php echo get_option('mpulse_api_key');?>" maxlength="29" placeholder="A1A1A-B2B2B-C3C3C-D4D4D-E5E5E"/>
 			<input name="submit" type="submit" class="soasta_submit"/>
@@ -71,31 +78,31 @@ function mpulse_plugin_page() {
 //Include the snippet in the head of each page
 function mpulse_add_rum_header() {
 ?>    
-  <script type='text/javascript'>(function(){
-  var dom,doc,where,iframe = document.createElement('iframe');
-  iframe.src = "javascript:false";
-  (iframe.frameElement || iframe).style.cssText = "width: 0; height: 0; border: 0";
-  where = document.getElementsByTagName('script')[0];
-  where.parentNode.insertBefore(iframe, where);
-  try {
-    doc = iframe.contentWindow.document;
-  } catch(e) {
-    dom = document.domain;
-    iframe.src="javascript:var d=document.open();d.domain='"+dom+"';void(0);";
-    doc = iframe.contentWindow.document;
-  }
-  doc.open()._l = function() {
-    var js = this.createElement("script");
-    if(dom) this.domain = dom;
-    js.id = "boomr-if-as";
-    js.src = '//c.go-mpulse.net/boomerang/' +
-    '<?php echo get_option('mpulse_api_key');?>';
-    BOOMR_lstart=new Date().getTime();
-    this.body.appendChild(js);
-  };
-  doc.write('<body onload="document._l();">');
-  doc.close();
-  })();</script>;
+	<script type='text/javascript'>(function(){
+	var dom,doc,where,iframe = document.createElement('iframe');
+	iframe.src = "javascript:false"; iframe.title = ""; iframe.role="presentation";
+	(iframe.frameElement || iframe).style.cssText = "width: 0; height: 0; border: 0";
+	where = document.getElementsByTagName('script')[0];
+	where.parentNode.insertBefore(iframe, where);
+	try {
+		doc = iframe.contentWindow.document;
+	} catch(e) {
+	dom = document.domain;
+	iframe.src="javascript:var d=document.open();d.domain='"+dom+"';void(0);";
+	doc = iframe.contentWindow.document;
+	}
+	doc.open()._l = function() {
+	var js = this.createElement("script");
+	if(dom) this.domain = dom;
+	js.id = "boomr-if-as";
+	js.src = '//c.go-mpulse.net/boomerang/' +
+	'<?php echo get_option('mpulse_api_key');?>';
+	BOOMR_lstart=new Date().getTime();
+	this.body.appendChild(js);
+	};
+	doc.write('<body onload="document._l();">');
+	doc.close();
+	})();</script>;
 
 <?php
 }
@@ -108,11 +115,4 @@ add_filter('plugin_action_links', 'mpulse_plugin_action_links', 10, 2);
 if ( function_exists('register_uninstall_hook') ){
 	register_uninstall_hook(__FILE__, 'mpulse_cleanup');
 }
-
-// Make sure we don't expose any info if called directly
-if ( !function_exists( 'add_action' ) ) {
-	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
-	exit;
-}
-
 ?>
